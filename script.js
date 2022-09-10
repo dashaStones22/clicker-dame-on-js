76b88d61d7b7faeb46d974220f87690023446ca1
Dasha <dashastones22@gmail.com>
2022-09-10 13:41:34 +0600

    add css styles

---
+++ script.js
@@ -0,0 +1,7 @@
+let time_header = document.querySelector('#time-header')
+let result_header = document.querySelector('#result-header')
+let start = document.querySelector('start')
+let game_field = document.querySelector('#game-field')
+let game_time = document/querySelector('#game-time')
+let time = document.querySelector('#time')
+let result = document.querySelector('#result')
